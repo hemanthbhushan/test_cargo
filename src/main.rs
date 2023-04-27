b9aa57564d8d44f1024a56ede41f04316001e3eb
Hemanth Bhushan <hemanth.bhushan@antiersolutions.com>
2023-04-27 11:38:00 +0530

    commit message

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,40 @@
 use std::io;
 fn main() {
-    let _x =  String::new();
-    let mut x = String::new();
-    println!("enter the value" );
+    let arr = [1, 23, 4, 5, 6, 7];
 
-    io::stdin().read_line(&mut x).expect("wrong");
-    println!("You guessed: {x}");
+    let mut index = String::new();
+    let mut ele = 0;
+    println!("enter the number ");
 
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+   if  index < arr.len()-1 {
+     ele = arr[index];
+     println!("the element is {}", ele);
+   }else {
+       println!("out of boundaryyy");
+   }
+   
+
+    // let mut count = 0;
+    // loop {
+    //     if count < arr.len() {
+    //         println!("{}", arr[count]);
+    //     }
+    //     count += 1;
+
+    //     if count > arr.len() {
+    //         break;
+    //     }
+    // }
+
+    //  for ele in &arr{
+    //     println!("{}",ele);
+    //  }
 }
